0ae7401f669ac50791aba9877b986d3b6d110f85
rh742831388 <742831388@qq.com>
2015-06-10 16:39:02 +0800

    Output

---
+++ obj/local/armeabi/objs/iw/iwlib.o.d
@@ -0,0 +1,278 @@
+/root/下载/wireless-tools-android//obj/local/armeabi/objs/iw/iwlib.o: \
+ //root/下载/wireless-tools-android/iwlib.c \
+ //root/下载/wireless-tools-android/iwlib.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/ioctl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/ioctl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/ioctl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/ioctl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/ioctls.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/termbits.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/ioctl_compat.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/math.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/limits.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/page.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/errno.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/errno.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/errno.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/signal.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/string.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/ctype.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/netdb.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/socket.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/socket.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/socket.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/sockios.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/sockios.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/uio.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/ethernet.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/if_ether.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/if_ether.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/ethertypes.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/if_arp.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/if_arp.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/netdevice.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/if.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/hdlc/ioctl.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/if_packet.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/netinet/in.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/endian.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/endian.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/in.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/byteorder.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/in6.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/ipv6.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/netinet/in6.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/netinet/if_ether.h \
+ /root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/if.h \
+ //root/下载/wireless-tools-android/wireless.h
+
+//root/下载/wireless-tools-android/iwlib.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/ioctl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/ioctl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/ioctl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/ioctl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/ioctls.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/termbits.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/ioctl_compat.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/math.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/page.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/errno.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/errno.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/errno.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/errno.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/string.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/ctype.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/netdb.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/socket.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/socket.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/socket.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/sockios.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/sockios.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/uio.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/ethernet.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/if_ether.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/if_ether.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/ethertypes.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/if_arp.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/if_arp.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/netdevice.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/if.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/hdlc/ioctl.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/if_packet.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/netinet/in.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/machine/endian.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/in.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/asm/byteorder.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/byteorder/little_endian.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/byteorder/swab.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/byteorder/generic.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/in6.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/linux/ipv6.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/netinet/in6.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/netinet/if_ether.h:
+
+/root/android/android-ndk-r8e/platforms/android-3/arch-arm/usr/include/net/if.h:
+
+//root/下载/wireless-tools-android/wireless.h:
